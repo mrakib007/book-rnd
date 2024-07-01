84443a16fbd4ef94ec8e8667221ebe158315d4f9
mrakib007 <mrakibhasan199744@gmail.com>
2024-07-01 14:47:16 +0600

    Footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,23 +2,21 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../pages/HomePage/Home";
 
-
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-                
-            },
-            
-        ]
-    },{
-        path: 'login',
-        // element: <Login></Login>
-    }
-])
+   {
+      path: "/",
+      element: <Main />,
+      children: [
+         {
+            path: "/",
+            element: <Home />,
+         },
+      ],
+   },
+//    {
+//       path: "login",
+//       element: <Login></Login>,
+//    },
+]);
 
 export default router;
