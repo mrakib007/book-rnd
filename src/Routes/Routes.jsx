b9ad858013c608068590e1e15812c22f49a5eae6
mrakib007 <mrakibhasan199744@gmail.com>
2024-07-01 12:01:26 +0600

    layout fixation

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layouts/Main";
+import Home from "../pages/HomePage/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+                
+            },
+            
+        ]
+    },{
+        path: 'login',
+        // element: <Login></Login>
+    }
+])
+
+export default router;
